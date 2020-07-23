602b7a5dc0fad658c099812bbcfefa7bd75f50bf
Naoki jarinosuke ISHIKAWA <naokiishikawa0808@gmail.com>
2020-07-23 09:09:49 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,41 +7,34 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+ const siteTitle = data.site.siteMetadata.title
+ const posts = data.allMarkdownRemark.edges
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-    </Layout>
-  )
+ return (
+   <Layout location={location} title={siteTitle}>
+     <SEO title="All posts" />
+     <Bio />
+     {posts.map(({ node }) => {
+       const title = node.frontmatter.title || node.fields.slug
+       return (
+         <article key={node.fields.slug}>
+           <header>
+             <h3
+               style={{
+                 marginBottom: rhythm(1 / 20),
+               }}
+             >
+               <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                 {title}
+               </Link>
+             </h3>
+             <small>{node.frontmatter.date}</small>
+           </header>
+         </article>
+       )
+     })}
+   </Layout>
+ )
 }
 
 export default BlogIndex
