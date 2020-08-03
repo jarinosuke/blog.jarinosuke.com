a3c60291cd9bce4101347b41ebd02e46633b8e02
Naoki jarinosuke ISHIKAWA <naokiishikawa0808@gmail.com>
2020-08-03 15:03:05 +0900

    display tags sample page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,12 @@
 const path = require(`path`)
+const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const tagTemplate = path.resolve("src/templates/tags.js")
   const result = await graphql(
     `
       {
@@ -22,6 +24,11 @@ exports.createPages = async ({ graphql, actions }) => {
               }
             }
           }
+          tags: allMarkdownRemark(limit: 1000) {
+            group(field: frontmatter___tags) {
+              fieldValue
+            }
+          }
         }
       }
     `
@@ -35,16 +42,24 @@ exports.createPages = async ({ graphql, actions }) => {
   const posts = result.data.allMarkdownRemark.edges
 
   posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
     createPage({
       path: post.node.fields.slug,
       component: blogPost,
       context: {
         slug: post.node.fields.slug,
-        previous,
-        next,
+      },
+    })
+  })
+
+  // Extract tag data from query
+  const tags = result.data.tagsGroup.group
+  // Make tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
       },
     })
   })
