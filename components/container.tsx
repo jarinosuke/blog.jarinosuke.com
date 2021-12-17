5d2ed52e3aae407e004d1bf50355159dc3133c3a
jarinosuke(Naoki ISHIKAWA) <naokiishikawa0808@gmail.com>
2021-12-17 14:56:57 +0900

    fix width

--- components/container.tsx
+++ components/container.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 const Container: FunctionComponent = ({ children }: Props) => {
-  return <div className="container md: mx-auto px-64">{children}</div>
+  return <div className="container md: mx-auto px-4">{children}</div>
 }
 
 export default Container
